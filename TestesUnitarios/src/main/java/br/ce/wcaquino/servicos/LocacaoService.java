06dda54509bfde4b8acbacdc3054eb3f5694b406
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-16 18:28:04 -0300

    Criando classe de teste no src de testes

--- TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -4,15 +4,16 @@ import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
+	
+	public String vPublica;
+	protected String vProtegida;
+	private String vPrivada;
+	String vDefault;
 
 	public Locacao alugarFilme(Usuario usuario, Filme filme) {
 		Locacao locacao = new Locacao();
@@ -30,24 +31,5 @@ public class LocacaoService {
 		// TODO adicionar mÃ©todo para salvar
 
 		return locacao;
-	}
-
-	@Test
-	public void teste() {
-
-		//cenário
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme 1", 2, 5.0);
-
-		//ação
-		Locacao locacao = service.alugarFilme(usuario, filme);
-		
-		//verificação
-		Assert.assertTrue(locacao.getValor() == 5.0);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-		
-
 	}
 }
