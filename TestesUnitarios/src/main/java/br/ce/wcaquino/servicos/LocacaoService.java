046a25d44ca698ea83547798e611c4e6018eb1f8
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-16 18:09:31 -0300

    Adicionando primeiro teste com JUnit

--- TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -4,12 +4,16 @@ import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
-	
+
 	public Locacao alugarFilme(Usuario usuario, Filme filme) {
 		Locacao locacao = new Locacao();
 		locacao.setFilme(filme);
@@ -17,18 +21,33 @@ public class LocacaoService {
 		locacao.setDataLocacao(new Date());
 		locacao.setValor(filme.getPrecoLocacao());
 
-		//Entrega no dia seguinte
+		// Entrega no dia seguinte
 		Date dataEntrega = new Date();
 		dataEntrega = adicionarDias(dataEntrega, 1);
 		locacao.setDataRetorno(dataEntrega);
-		
-		//Salvando a locacao...	
-		//TODO adicionar mÃ©todo para salvar
-		
+
+		// Salvando a locacao...
+		// TODO adicionar mÃ©todo para salvar
+
 		return locacao;
 	}
 
-	public static void main(String[] args) {
+	@Test
+	public void teste() {
+
+		//cenário
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
+
+		//ação
+		Locacao locacao = service.alugarFilme(usuario, filme);
 		
+		//verificação
+		Assert.assertTrue(locacao.getValor() == 5.0);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		
+
 	}
 }
