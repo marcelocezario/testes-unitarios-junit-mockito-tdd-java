f9d1fcc58bd5fcdfb558a349302cc78831ed7687
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-17 09:50:58 -0300

    Primeiros testes com JUNIT

---
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/AssertTest.java
@@ -0,0 +1,45 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Usuario;
+
+public class AssertTest {
+	
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+		Assert.assertFalse(false);
+		
+		Assert.assertEquals(1, 1);
+		Assert.assertEquals("Erro de comparacao", 1, 1);
+		Assert.assertEquals(0.51, 0.5199, 0.00999);
+		Assert.assertEquals(Math.PI, 3.14, 0.01);
+		
+		int i = 5;
+		Integer i2 = 5;
+		Assert.assertEquals(Integer.valueOf(i), i2);
+		Assert.assertEquals(i, i2.intValue());
+		
+		Assert.assertEquals("bola", "bola");
+		Assert.assertNotEquals("bola", "roupa");
+		Assert.assertTrue("bola".equalsIgnoreCase("Bola"));
+		Assert.assertTrue("bola".startsWith("bo"));
+		
+		Usuario u1 = new Usuario("Usuario 1");
+		Usuario u2 = new Usuario("Usuario 1");
+		Usuario u3 = u1;
+		Usuario u4 = null;
+		
+		Assert.assertEquals(u1, u2);
+		
+		// comparar instância
+		Assert.assertSame(u1, u3);
+		Assert.assertNotSame(u1, u2);
+		
+		Assert.assertNull(u4);
+		Assert.assertNotNull(u1);
+	}
+
+}
